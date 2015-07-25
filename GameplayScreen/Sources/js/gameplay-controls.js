c56e3ef01bb913de0fe3881561e1b6633ac9764d
vassildinev <vassildinev@gmail.com>
2015-07-25 14:05:19 +0300

    Added a few functions

--- GameplayScreen/Sources/js/gameplay-controls.js
+++ GameplayScreen/Sources/js/gameplay-controls.js
@@ -1,3 +1,27 @@
 window.onload = function () {
+    var CONSTANTS = {
+        STAGE_WIDTH: 800,
+        STAGE_HEIGHT: 600
+    };
 
+    var stage, layer;
+
+    function initialize() {
+        stage = new Kinetic.Stage({
+            container: 'gameplay-container',
+            width: CONSTANTS.STAGE_WIDTH,
+            height: CONSTANTS.STAGE_HEIGHT
+        });
+
+        layer = new Kinetic.Layer();
+    }
+
+    function run() {
+
+    }
+
+    (function () {
+        initialize();
+        run();
+    }());
 };

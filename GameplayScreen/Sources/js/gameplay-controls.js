862a096a7faf7bb8a7e773a3e1a52d0ef353f761
vassildinev <vassildinev@gmail.com>
2015-07-25 16:33:20 +0300

    Added player mouse movement

--- GameplayScreen/Sources/js/gameplay-controls.js
+++ GameplayScreen/Sources/js/gameplay-controls.js
@@ -1,25 +1,58 @@
 window.onload = function () {
     var CONSTANTS = {
         STAGE_WIDTH: 800,
-        STAGE_HEIGHT: 600
+        STAGE_HEIGHT: 600,
+
+        PLAYER_WIDTH: 150,
+        PLAYER_HEIGHT: 117
     };
 
-    var stage, layer, backgroundImageObj, playerImageObj;
+    var gameplayContainer, stage, backgroundLayer, actionLayer,
+        backgroundImageObj, playerImageObj,
+        playerDirection = 0,
+
+        player;
+
+    // 0: looking down, 1: looking up, 2: looking left, 3: looking right
 
     function loadCanvas() {
-        var container = document.getElementById('gameplay-container');
-        container.position =
+        gameplayContainer = document.getElementById('gameplay-container');
+        gameplayContainer.setAttribute('width', CONSTANTS.STAGE_WIDTH);
+        gameplayContainer.setAttribute('height', CONSTANTS.STAGE_HEIGHT);
+
         stage = new Kinetic.Stage({
             container: 'gameplay-container',
             width: CONSTANTS.STAGE_WIDTH,
             height: CONSTANTS.STAGE_HEIGHT
         });
 
-        layer = new Kinetic.Layer();
+        backgroundLayer = new Kinetic.Layer();
+        actionLayer = new Kinetic.Layer();
     }
 
     function loadPlayer() {
+        playerImageObj = new Image();
+        playerImageObj.src = "sources/images/player.png";
 
+        playerImageObj.onload = function () {
+            player = new Kinetic.Image({
+                x: 0,
+                y: 0,
+                image: playerImageObj,
+                width: CONSTANTS.PLAYER_WIDTH,
+                height: CONSTANTS.PLAYER_HEIGHT,
+                crop: {
+                    x: 0,
+                    y: 0,
+                    width: CONSTANTS.PLAYER_WIDTH,
+                    height: CONSTANTS.PLAYER_HEIGHT
+                }
+            });
+
+            addMouseEventListener(player);
+            actionLayer.add(player);
+            stage.add(actionLayer);
+        };
     }
 
     function loadBackground() {
@@ -35,9 +68,62 @@ window.onload = function () {
                 height: CONSTANTS.STAGE_HEIGHT
             });
 
-            layer.add(background);
-            stage.add(layer);
+            backgroundLayer.add(background);
+            stage.add(backgroundLayer);
         };
+    }
+
+    function addMouseEventListener(player) {
+        var playerCenterX = player.getX() + CONSTANTS.PLAYER_WIDTH / 2,
+            playerCenterY = player.getY() + CONSTANTS.PLAYER_HEIGHT / 2;
+
+        gameplayContainer.addEventListener('mousemove', function (e) {
+            if (e.clientX < playerCenterX) {
+                if (Math.abs(e.clientY - playerCenterY) < playerCenterX - e.clientX) {
+                    player.setCrop({
+                        x: 0, y: 2 * CONSTANTS.PLAYER_HEIGHT, width: CONSTANTS.PLAYER_WIDTH,
+                        height: CONSTANTS.PLAYER_HEIGHT
+                    });
+
+                    actionLayer.draw();
+                }
+            }
+
+            if (e.clientX > playerCenterX) {
+                if (Math.abs(e.clientY - playerCenterY) < e.clientX - playerCenterX) {
+                    player.setCrop({
+                        x: 0, y: 3 * CONSTANTS.PLAYER_HEIGHT, width: CONSTANTS.PLAYER_WIDTH,
+                        height: CONSTANTS.PLAYER_HEIGHT
+                    });
+
+                    actionLayer.draw();
+                }
+            }
+
+            if (e.clientY < playerCenterY) {
+                if (Math.abs(e.clientX - playerCenterX) < playerCenterY - e.clientY) {
+                    player.setCrop({
+                        x: 0, y: 1 * CONSTANTS.PLAYER_HEIGHT, width: CONSTANTS.PLAYER_WIDTH,
+                        height: CONSTANTS.PLAYER_HEIGHT
+                    });
+
+                    actionLayer.draw();
+                }
+            }
+
+            if (e.clientY > playerCenterY) {
+                if (Math.abs(e.clientX - playerCenterX) < e.clientY - playerCenterY) {
+                    player.setCrop({
+                        x: 0, y: 0 * CONSTANTS.PLAYER_HEIGHT, width: CONSTANTS.PLAYER_WIDTH,
+                        height: CONSTANTS.PLAYER_HEIGHT
+                    });
+
+                    actionLayer.draw();
+                }
+            }
+        });
+
+        console.log(playerCenterX);
     }
 
     function initialize() {

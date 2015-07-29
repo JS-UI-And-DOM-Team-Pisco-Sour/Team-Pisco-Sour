c3a49d191d94f3b465f8cfa2387be1870293b7d2
Petar Petrov <petar.kr.petrov@gmail.com>
2015-07-29 20:25:55 +0300

    fixes bugs with intro page layout; adds responsiveness; refactoring intropage html, css, js; adds minified/compressed background and hero images

--- IntroductionScreen/IntroControls.js
+++ IntroductionScreen/IntroControls.js
@@ -1,23 +1,20 @@
 function togglediv(id) {
     var div = document.getElementById(id),
-        gameControls = document.getElementById("game-controls"),
-        playButton = document.getElementById("playButton"),
+        gameControls = document.getElementById("game-controls"),        
         buttonSound = new Audio('audio/buttonClick.mp3');
 
     buttonSound.play();
     div.style.display = div.style.display == "none" ? "block" : "none";
-    gameControls.style.visibility =  gameControls.style.visibility == "visible" ? "hidden" : "visible";
+    gameControls.style.display =  gameControls.style.display == "block" ? "none" : "block";
 }
 
 function storeHeroName(id){
-        var input = window.document.getElementById(id).value;
-        if (input == '' || input == 'undefined') {
-            throw new Error("Player name is undefined or null");
-        }
-        else {
-            sessionStorage.setItem('heroName', input);
-        }
-            sessionStorage.playerScore=0;
-
+    var input = window.document.getElementById(id).value;
+    if (input == '' || input == 'undefined') {
+        throw new Error("Player name is undefined or null");
     }
+    else {
+        sessionStorage.setItem('heroName', input);
+    }
+}
 

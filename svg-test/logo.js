a34bd65396f0cfef89731832481576f7f98a110b
vassildinev <vassildinev@gmail.com>
2015-07-29 11:27:13 +0300

    Redesigned the team logo to fit the theme

--- svg-test/logo.js
+++ svg-test/logo.js
@@ -1,14 +1,14 @@
 window.onload = function () {
     var CONSTANTS = {
-        SVG_WIDTH: 400,
-        SVG_HEIGHT: 400,
+        SVG_WIDTH: 300,
+        SVG_HEIGHT: 300,
         SVG_TOP_LEFT_X: 50,
         SVG_TOP_LEFT_Y: 50,
 
         OUTLINE_RADIUS: 20,
         OUTLINE_STROKE_WIDTH: 15,
 
-        SHIP_SCALE: 3,
+        SHIP_SCALE: 2,
 
         BEZIER_CIRCLE_CONST: 0.551915,
         MAX_NUMBER_OF_LOOPS: 100,
@@ -32,23 +32,47 @@ window.onload = function () {
         paper = Snap('#svg_logo');
     }
 
+    function loadFont() {
+        var logoTextStyle = document.createElement('style');
+        logoTextStyle.appendChild(document.createTextNode(
+            "@font-face {" +
+                "font-family: 'logoFont';" +
+                "src: url('./th3-machine.ttf');" +
+            "}"));
+
+        document.head.appendChild(logoTextStyle);
+    }
+
     function drawOutline() {
+        var pattern = paper.path("M10-5-10,15M5-10-5,10M20-15-20,25").attr({
+            'fill': 'red',
+            'strokeWidth': 3,
+            'stroke': '#AAA'
+        });
+
+        pattern = pattern.pattern(0, 0, 5, 5);
         paper.rect(CONSTANTS.OUTLINE_RADIUS / 2, CONSTANTS.OUTLINE_RADIUS / 2,
             CONSTANTS.SVG_WIDTH - CONSTANTS.OUTLINE_RADIUS, CONSTANTS.SVG_HEIGHT - CONSTANTS.OUTLINE_RADIUS,
             CONSTANTS.OUTLINE_RADIUS, CONSTANTS.OUTLINE_RADIUS)
             .attr({
-                'stroke': '#123456',
+                'stroke': pattern,
                 'strokeWidth': CONSTANTS.OUTLINE_STROKE_WIDTH,
                 'fill': 'none',
-                'opacity': 0.3
+                'opacity': 0.7
             });
     }
 
     function drawSpaceship() {
         // Legs
+        var pattern = paper.path("M10-5-10,15M15,0,0,15M0-5-20,15").attr({
+            fill: "#D13A00",
+            stroke: "#C48368",
+            strokeWidth: 5
+        });
+        pattern = pattern.pattern(0, 0, 1, 6);
         paper.path('M38,36.013l-6,6l-4,4v6l-6,10l4,2l14-14L38,36.013z M28,26.013l-14-2l-14,14l2,4l10-6h6l4-4L28,26.013z')
             .attr({
-                'fill': '#CCC'
+                'fill': pattern
             })
             .transform('t' + 135 / 400 * CONSTANTS.SVG_WIDTH + ',' + 185 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
 
@@ -57,28 +81,35 @@ window.onload = function () {
             .attr({
                 'fill': '#FC6'
             })
-            .transform('t' + 115 / 400 * CONSTANTS.SVG_WIDTH + ',' + 205 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
+            .transform('t' + 130 / 400 * CONSTANTS.SVG_WIDTH + ',' + 190 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
 
         // Inner flames
         paper.path('M16,42.013c-3.939,5.748-12,12.835-12,16c0,2.091,0.201,2,2,2c3.217,0,10.011-8.031,16-12 L16,42.013z')
             .attr({
                 'fill': '#ED7161'
             })
-            .transform('t' + 120 / 400 * CONSTANTS.SVG_WIDTH + ',' + 200 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
+            .transform('t' + 130 / 400 * CONSTANTS.SVG_WIDTH + ',' + 190 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
 
         // Body below
         paper.path('M60,0.013c-6.286,0.389-17.138,1.137-30,14C20.539,23.474,12.239,37.231,8.348,46.36l9.367,9.367 ' +
             'C26.793,51.874,40.459,43.553,50,34.013c12.779-12.779,13.507-23.669,14-30C64.22,1.187,62.614-0.149,60,0.013z')
             .attr({
-                'fill': '#387AA7'
+                'fill': '#597A43'
             })
             .transform('t' + 170 / 400 * CONSTANTS.SVG_WIDTH + ',' + 150 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
 
         // Body above
+        pattern = paper.path("M20-10-20,40M30,10,10,15M0-5-20,15").attr({
+            fill: "none",
+            stroke: "#B8BC62",
+            strokeWidth: 5
+        });
+
+        pattern = pattern.pattern(5, 5, 10, 10);
         paper.path('M60,0.013c-6.286,0.389-17.138,1.137-30,14c-7.724,7.723-14.664,18.307-19.078,26.905 l12.235,' +
             '12.235C31.703,48.751,42.222,41.791,50,34.013c12.779-12.779,13.507-23.669,14-30C64.22,1.187,62.614-0.149,60,0.013z')
             .attr({
-                'fill': '#48A0DC'
+                'fill': pattern
             })
             .transform('t' + 172.5 / 400 * CONSTANTS.SVG_WIDTH + ',' + 147.5 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
 
@@ -89,7 +120,7 @@ window.onload = function () {
 
         // Inner glass
         paper.circle(48, 16.013, 4).attr({
-            'fill': '#FFFFFF'
+            'fill': '#7CBDF9'
         }).transform('t' + 192 / 400 * CONSTANTS.SVG_WIDTH + ',' + 128 / 400 * CONSTANTS.SVG_HEIGHT + 's' + CONSTANTS.SHIP_SCALE + ',' + CONSTANTS.SHIP_SCALE);
     }
 
@@ -112,8 +143,10 @@ window.onload = function () {
 
         var animatedText = paper.text(0, 0, teamName)
             .attr({
-                'font': '30px Consolas',
-                'textpath': path
+                'font-family': 'logoFont',
+                'font-size': '30px',
+                'textpath': path,
+                'fill': '#FFF'
             })
             .transform('t' + CONSTANTS.SVG_WIDTH / 2 + ',' + CONSTANTS.SVG_HEIGHT / 2);
 
@@ -126,8 +159,9 @@ window.onload = function () {
     }
 
     (function () {
+        loadFont();
         loadSVG();
-        drawOutline();
+        //drawOutline();
         drawSpaceship();
         drawTeamName();
     }())

8aa488c2cd71e7abceb28612b6cb792b26385dc6
vassildinev <vassildinev@gmail.com>
2015-07-31 01:42:34 +0300

    Reorganized the intro dirs

--- intro/assets/js/intro.js
+++ intro/assets/js/intro.js
@@ -1,14 +1,14 @@
 function togglediv(id) {
     var div = document.getElementById(id),
-        gameControls = document.getElementById("game-controls"),        
-        buttonSound = new Audio('audio/buttonClick.mp3');
+        gameControls = document.getElementById("game-controls"),
+        buttonSound = new Audio('assets/audio/buttonClick.mp3');
 
     buttonSound.play();
     div.style.display = div.style.display == "none" ? "block" : "none";
-    gameControls.style.display =  gameControls.style.display == "block" ? "none" : "block";
+    gameControls.style.display = gameControls.style.display == "block" ? "none" : "block";
 }
 
-function storeHeroName(id){
+function storeHeroName(id) {
     var input = window.document.getElementById(id).value;
     if (input == '' || input == 'undefined') {
         throw new Error("Player name is undefined or null");

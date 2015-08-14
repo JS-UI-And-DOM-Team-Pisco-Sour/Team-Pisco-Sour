34b52ba7482404591ffa76856f5a56df90f24fb4
zdzdz <zdzdz.link@yahoo.com>
2015-08-14 12:10:46 +0300

    1.Removed margin from the health bar because it breaks in some browsers and resolutions. 2.Changed the path to gameplay.html from localhost to relative path.

--- intro/assets/js/intro.js
+++ intro/assets/js/intro.js
@@ -4,19 +4,19 @@ function togglediv(id) {
         buttonSound = new Audio('assets/audio/buttonClick.mp3');
 
     buttonSound.play();
-    div.style.display = div.style.display == "none" ? "block" : "none";
-    gameControls.style.display = gameControls.style.display == "block" ? "none" : "block";
+    div.style.display = div.style.display === "none" ? "block" : "none";
+    gameControls.style.display = gameControls.style.display === "block" ? "none" : "block";
 }
 
 function storeHeroName(id) {
     var input = window.document.getElementById(id).value;
-    if (input == '' || input == 'undefined') {
+    if (input === '' || input === 'undefined') {
         throw new Error("Player name is undefined or null");
     }
     else {
         sessionStorage.setItem('heroName', input);
     }
 
-    window.location.href = "http://localhost:63342/Team-Pisco-Sour/gameplay/gameplay.html";
+    window.location.href = "../gameplay/gameplay.html";
 }
 
